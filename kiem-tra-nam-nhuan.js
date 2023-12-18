0dc9a3fcb116f869108e09da32cf2e47e70a2e52
nhathuynguyenhai1999 <129018441+nhathuynguyenhai1999@users.noreply.github.com>
2023-12-18 16:56:12 +0700

    Update kiem-tra-nam-nhuan.js

--- kiem-tra-nam-nhuan.js
+++ kiem-tra-nam-nhuan.js
@@ -6,20 +6,3 @@ function year(){
   } else {
     alert(namnhuan + " không là năm nhuận.");
   }
-} // đây là hàm function
-
-
-
-// if (namnhuan % 4 == 0) {
-//   if (namnhuan % 100 == 0) {
-//     if (namnhuan % 400 == 0) {
-//       alert(namnhuan + " is a leap year");
-//     } else {
-//       alert(namnhuan + " is NOT a leap year");
-//     }
-//   } else {
-//     alert(namnhuan + " is a leap year");
-//   }
-// } else {
-//   alert(namnhuan + " is NOT a leap year");
-// }
